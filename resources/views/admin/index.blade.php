bbc2aad3ce6f6c775a0fd7f37ed851e35537bf50
Dennis van Schie <1998dennis@live.nl>
2019-11-07 22:36:12 +0100

    removed a file

--- resources/views/admin/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<a href="{{route('admin.users')}}" class="btn btn-primary">Alle gebruikers</a>
-<a href="{{route('admin.restaurants')}}" class="btn btn-primary">Alle restaurants</a>
-<a href="{{route('admin.consumables')}}" class="btn btn-primary">Alle Alle versnaperingen</a>
-@endsection
